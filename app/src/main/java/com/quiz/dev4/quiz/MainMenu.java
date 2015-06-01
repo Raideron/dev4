a9272369cca69be6a9b4dd0d5ced1b58d0cdef56
Raideron <0901267@hr.nl>
2015-06-01 11:24:08 +0200

    start

---
+++ app/src/main/java/com/quiz/dev4/quiz/MainMenu.java
@@ -0,0 +1,73 @@
+package com.quiz.dev4.quiz;
+
+import android.content.Intent;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainMenu extends ActionBarActivity {
+
+    String tag = MainMenu.class.getSimpleName();
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(tag, "On start");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_menu, menu);
+        return true;
+    }
+
+    public void loadQuestion(View v) {
+        Button button = (Button) v;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void startQuiz(View v) {
+        Log.d("test1", "button is clicked");
+        Intent myIntent = new Intent(MainMenu.this, Question.class);
+        //myIntent.putExtra("key", value); //Optional parameters
+        MainMenu.this.startActivity(myIntent);
+    }
+}
---
+++ app/src/main/java/com/quiz/dev4/quiz/Question.java
@@ -0,0 +1,49 @@
+package com.quiz.dev4.quiz;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class Question extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_question, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void startResults(View v) {
+        Log.d("test1", "button is clicked");
+        Intent myIntent = new Intent(Question.this, Result.class);
+        //myIntent.putExtra("key", value); //Optional parameters
+        Question.this.startActivity(myIntent);
+    }
+}
